a0296bf6aced44aad86d2e8f5c73c73c1ed2c66a
Saifuddin575408 <sksaifuddin575@gmail.com>
2023-04-05 17:10:05 +0530

    Java Features

---
+++ src/main/java/com/app/feattures/JavaFeaturesAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.app.feattures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeaturesAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFeaturesAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/feattures/JavaFeaturesAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.feattures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFeaturesAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

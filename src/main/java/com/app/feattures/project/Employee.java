b360cdc9e97034adad2bb42967d8f7908ae935c9
Saifuddin575408 <sksaifuddin575@gmail.com>
2023-05-29 16:14:30 +0530

    Employee Stream Api Function Implemented

---
+++ src/main/java/com/app/feattures/project/Employee.java
@@ -0,0 +1,104 @@
+package com.app.feattures.project;
+
+public class Employee {
+
+	
+	
+	public Integer id;
+
+	public String name;
+
+	public Integer age;
+
+	public String gender;
+
+	public String department;
+
+	public Integer yearOfJoining;
+
+	public Double salary;
+
+	public Employee() {
+		super();
+	}
+	
+	public Employee(Integer id, String name, Integer age, String gender, String department, Integer yearOfJoining,
+			Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Integer getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public void setYearOfJoining(Integer yearOfJoining) {
+		this.yearOfJoining = yearOfJoining;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
+	}
+
+	
+
+
+	
+	
+}
